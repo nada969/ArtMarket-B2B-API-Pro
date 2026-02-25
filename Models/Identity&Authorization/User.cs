46740263a91bc7358e8b445c14fca388e87c26dd
nada969 <nadamousa969@gmail.com>
2026-02-25 23:41:26 +0200

    set structure folders

--- Models/Identity&Authorization/User.cs
+++ Models/Identity&Authorization/User.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace B2B_Procurement___Order_Management_Platform.Models.Identity_Authorization
 {
     public class User
     {
         public int Id { get; set; }
-
+        [RegularExpression(@"[a-z]+\@gmail+\.com")]
         public string Email { get; set; } = default!;
         public string Password { get; private set; } 
         public bool IsActive { get; set; } = true;
